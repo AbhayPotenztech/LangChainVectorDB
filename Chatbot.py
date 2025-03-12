844d2e2f9e3f745b732bd7a019ae0cd9540e918c
AbhayPotenztech <abhay@potenztech.com>
2025-03-12 15:37:47 +0530

    LangChain

---
+++ Chatbot.py
@@ -0,0 +1,35 @@
+import getpass
+import os
+
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+
+_set_env("OPENAI_API_KEY")
+from typing import List
+
+from langchain_core.messages import SystemMessage
+from langchain_openai import ChatOpenAI
+
+from pydantic import BaseModeldef get_messages_info(messages):
+    return [SystemMessage(content=template)] + messages
+
+
+class PromptInstructions(BaseModel)
+
+    objective: str
+    variables: List[str]
+    constraints: List[str]
+    requirements: List[str]
+
+
+llm = ChatOpenAI(temperature=0)
+llm_with_tool = llm.bind_tools([PromptInstructions])
+
+
+def info_chain(state):
+    messages = get_messages_info(state["messages"])
+    response = llm_with_tool.invoke(messages)
+    return {"messages": [response]}
