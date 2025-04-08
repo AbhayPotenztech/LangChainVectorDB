e32e61ab6a24f33c95b8a9c1a87e04c0995b58b4
AbhayPotenztech <abhay@potenztech.com>
2025-04-08 11:18:06 +0530

    Delete import os.py

--- import os.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Check if the OPENAI_API_KEY environment variable is set
-api_key = os.environ.get('OPENAI_API_KEY')
-
-if api_key:
-    print(f"API Key is set: {api_key}")
-else:
-    print("API Key is NOT set!")
